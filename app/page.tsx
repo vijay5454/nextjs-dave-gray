31b2d11121b375c9113324d8d150c103be9590c9
vijay5454 <vijaythalaiva5454@gmail.com>
2025-01-31 20:40:52 +0530

    Add icon for brand.

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="bg-home-img h-[100vh] bg-cover bg-center relative">
       <div className="bg-black/50 absolute inset-0 flex justify-center items-center">
         <div>
-          <h2 className="font-bold text-xl">Vijay's Repair Shop</h2>
+          <h2 className="font-bold text-xl">Vijay&apos;s Repair Shop</h2>
           <address>31/22, Rajakilpakkam, Tambaram, Chennai - 600073</address>
           <h3 className="font-semibold">Contact Information</h3>
           <p>
